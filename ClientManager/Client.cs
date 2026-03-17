9338e0279d900a861f5f701dccb599dd6aebf64b
Кулешов Илья <232909071095-15>
2026-03-17 10:29:03 +0300

    2. Добавлен класс Client

---
+++ ClientManager/Client.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientManager
+{
+    public class Client
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+
+        public Client(string name, string email, string phone, string address) 
+        { 
+            Name = name;
+            Email = email;
+            Phone = phone;
+            Address = address;
+        }
+    }
+}
